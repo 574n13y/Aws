2bcce0e9470bdfa56db3f064f12ffe74e26d13a6
VIVESH TYAGI <35293085+574n13y@users.noreply.github.com>
2023-10-23 04:50:06 +0530

    Update ec2.tf

--- Host a static website Windows IIS/ec2.tf
+++ Host a static website Windows IIS/ec2.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "terraform-cloud-vm" {
-  ami                    = data.aws_ami.amzlinux.id
+  ami                    = data.aws_ami.windows-ami.id
   instance_type          = var.instance_type
   count                  = 1
   key_name               = "terraform-cloud-key"
-  user_data              = file("install.sh")
   vpc_security_group_ids = [aws_security_group.terraform-cloud-vm-sg.id,]
   tags = {
     "Name" = "terraform-cloud-vm-${count.index}"

d34e5acdefa968abd7856b9e2a2643df7618c24d
VIVESH TYAGI <35293085+574n13y@users.noreply.github.com>
2023-10-23 04:35:54 +0530

    Create security-group.tf

---
+++ Host a static website Windows IIS/security-group.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "terraform-cloud-vm-sg" {
+  name        = "terraform-cloud-vm-sg"
+  description = "terraform-cloud-vm-sg"
+  ingress {
+    description = "allow 22 port"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "allow 80 port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "outbound all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

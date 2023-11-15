73527385d4d7c44a38aba1372afb4dd19519875f
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-11-15 14:02:19 +0530

    Create output.tf

---
+++ aws infrastructure/output.tf
@@ -0,0 +1,11 @@
+output "availability_zone_a" {
+  value = "ap-northeast-1a"
+}
+
+output "availability_zone_b" {
+  value = "ap-northeast-1c"
+}
+
+output "availability_zone_c" {
+  value = "ap-northeast-1d"
+}

df4131f1807de16876643b1184fb8403309d9a86
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-11-15 14:11:00 +0530

    Create elastic_ip_output.tf

---
+++ aws infrastructure/elastic_ip_output.tf
@@ -0,0 +1,7 @@
+output "elastic_ip_nat_gateway_1c_id" {
+  value = aws_eip.elastic_ip_nat_gateway_1c.id
+}
+
+output "elastic_ip_nat_gateway_1d_id" {
+  value = aws_eip.elastic_ip_nat_gateway_1d.id
+}

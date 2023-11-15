453f8f78ef80fd17b919b0ced44b47bfa52ec5cc
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-11-15 14:21:53 +0530

    Create ng_variables.tf

---
+++ aws infrastructure/ng_variables.tf
@@ -0,0 +1,18 @@
+variable "allocation_nat_gateway_1c_id" {
+  description = "allocation_nat_gateway_1c_id"
+  type        = string
+}
+variable "allocation_nat_gateway_1d_id" {
+  description = "allocation_nat_gateway_1d_id"
+  type        = string
+}
+
+variable "subnet_public_c_id" {
+  description = "subnet_public_c_id"
+  type        = string
+}
+
+variable "subnet_public_d_id" {
+  description = "subnet_public_d_id"
+  type        = string
+}
